943474b1cb405b754cf50338b80578d8b08540da
NecRaul <necraul@kuroneko.dev>
2026-06-17 09:35:15 +0400

    Replacement class

---
+++ replacement.py
@@ -0,0 +1,6 @@
+class Replacement:
+    def __init__(self, line, original, replacement, comment=""):
+        self.line = line
+        self.original = original
+        self.replacement = replacement
+        self.comment = comment
