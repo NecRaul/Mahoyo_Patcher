7b2192e0fb9bcf80e778b9483609f972f57ec419
NecRaul <necraul@kuroneko.dev>
2026-06-17 10:49:47 +0400

    Clean up replacer.py

--- replacer.py
+++ replacer.py
@@ -1,129 +1,6 @@
 import os
 import re
 
-def metric(text):
-    indices = [89, 113, 1482, 1602, 1604,
-               1874, 1877, 2576, 2991, 3083,
-               3101, 3156, 4012, 4059, 4419,
-               4476, 4553, 4558, 4655,
-               4695, 4837, 4849, 4852, 4858,
-               4877, 4940, 4941, 5156, 5214,
-               5565, 5613, 5688, 5866,
-               5879, 5890, 5892, 5909,
-               5957, 5962, 5965, 6012, 6300,
-               6364, 6376, 6400, 6407,
-               6449, 6478, 6491, 6826, 6838,
-               6918, 6921, 7105,
-               7227, 7238, 7243, 7308, 5826, 8772,
-               10175, 10176, 10192, 10647,
-               11009, 11047, 11727, 14791, 14988,
-               15286, 15288, 15381, 15895,
-               16798, 16827, 16916, 16923, 16974,
-               17218, 17487,
-               17515, 17549, 17880, 18038, 18070,
-               18308,
-               18328, 18329, 18340, 18383, 20479,
-               20786, 20930, 21087, 22576, 22577, 23065,
-               14811, 4486, 1485,
-               11505,
-               3955, 6007, 11727, 13055]
-    imperial_strings = ["43 degrees Fahrenheit", "thirty-foot", "over three feet", "45 miles", "miles",
-                        "yards", "15 feet", "was a good few feet in length", "6 feet", "a few feet",
-                        "ten feet", "a few feet", "yards", "thirty feet", "greater than a football field",
-                        "fifteen feet", "few feet", "sixty feet", "thirty or so feet",
-                        "a couple hundred feet", "thirty-foot", "hundred-pound", "thirty feet", "thirty feet",
-                        "thirty feet", "a hundred feet", "fifty feet", "thirty feet", "thirty feet",
-                        "thirty feet", "yards", "fifty feet", "one hundred feet",
-                        "One hundred and fifty feet", "fifteen feet", "foot", "a hundred and fifty feet",
-                        "two hundred feet", "three hundred feet", "half-mile", "650 yards", "miles",
-                        "a great height―was", "six hundred feet", "two hundred feet", "two-hundred-foot",
-                        "five feet", "a hundred feet", "a few feet", "sixty-foot", "yards...!",
-                        "the better part of a mile", "mile", "two hundred and fifty feet",
-                        "yards", "ten feet", "three feet", "paces", "three feet", "couple of feet",
-                        "sixteen inches", "sixty miles", "six miles", "three hundred feet",
-                        "yards", "inches", "112 miles", "twelve feet", "eighty feet",
-                        "ten miles", "Four inches", "six miles", "six feet",
-                        "two miles", "a hundred feet", "yards", "yards", "a hundred feet",
-                        "closed the distance between them.", "thirty feet",
-                        "Fifteen feet to go", "Five feet", "twenty pounds", "feet", "twenty-foot",
-                        "the beast now towered above him",
-                        "thirty feet", "thirty feet", "three feet", "three feet", "seventy-pound",
-                        "yards", "yards", "thirty feet", "miles", "50 miles", "eight inches",
-                        "twelve hundred square foot", "a foot to his left", "a foot and a half",
-                        "myself, but I think my brain had a major malfunction, and my hand just went for it!",
-                        "more than eighty-five square acres", "238,900 miles", "38 miles", "ounce"]
-    metric_strings = ["6 degrees Celsius", "ten-meter", "around one meter", "70 kilometers", "kilometers",
-                      "meters", "five meters", "stretched out several meters", "two meters", "one meter",
-                      "several meters", "one meter", "meters", "ten meters", "about a hundred meters",
-                      "five meters", "two meters", "twenty meters", "ten or so meters",
-                      "about sixty meters", "ten-meter", "forty-five-kilo", "ten meters", "ten meters",
-                      "ten meters", "thirty meters", "fifteen meters", "ten meters", "ten meters",
-                      "ten meters", "meters", "twenty meters", "forty meters",
-                      "Forty-eight meters", "five meters", "meter", "fifty meters",
-                      "sixty meters", "a hundred meters", "one-kilometer", "600 meters", "kilometers",
-                      "sixty meters above ground―was", "two hundred meters", "sixty meters", "sixty-meter",
-                      "two meters", "thirty meters", "about a meter", "18-meter", "meters...!",
-                      "about a kilometer", "kilometer", "eighty meters",
-                      "meters", "three meters", "a meter", "meters", "a meter", "meter",
-                      "forty centimeters", "a hundred kilometers", "ten kilometers", "a hundred meters",
-                      "meters", "centimeters", "180 kilometers", "six meters", "eight meters",
-                      "fifteen kilometers", "Ten centimeters", "ten kilometers", "two meters",
-                      "three kilometers", "thirty meters", "meters", "meters", "thirty meters",
-                      "closed the distance between them to just ten meters.", "ten meters",
-                      "Five meters to go", "Two meters", "ten kilos", "meters", "six-meter",
-                      "the boy, who was smaller than him, had grown to a size of nearly two meters",
-                      "ten meters", "ten meters", "a meter", "a meter", "thirty-kilo",
-                      "meters", "meters", "ten meters", "kilometers", "80 kilometers", "twenty centimeters",
-                      "four hundred square meters", "twenty centimeters", "fifty centimeters",
-                      "just a centimeter before that, but I misjudged. It turns out that Aozaki's physical size was slightly greater than my visual information.",
-                      "350,000 square meters", "384,400 kilometers", "60 kilometers", "gram"]
-
-    for i in range (len(indices)):
-        text[indices[i]-1] = text[indices[i]-1].replace(imperial_strings[i], metric_strings[i])
-
-    return text
-
-def romanization(text):
-    localized_name = ["Soujyuro", "Sizuki", "Fumidsuka", "Yamasiro", "Koga"]
-    hepburn_name = ["Soujuurou", "Shizuki", "Fumizuka", "Yamashiro", "Kouga"]
-
-    for i in range (len(localized_name)):
-        line_count = 0
-        for line in text:
-            if (localized_name[i] in line) and (hepburn_name[i] not in line):
-                text[line_count] = text[line_count].replace(localized_name[i], hepburn_name[i])
-            line_count += 1
-
-    # Edge Cases
-    # "The 'jyu' in your name"
-    text[1917] = text[1917].replace("jyu", "juu")
-    # Si -> Shi
-    text[15548] = text[15548].replace("Si", "Shi")
-    # Sou...jyuro... -> Sou...juurou...
-    text[18757] = text[18757].replace("jyuro", "juurou")
-    # Touko -> Toko ### It's how Ritsuka calls Touko
-    text[20888] = text[20888].replace("Touko", "Toko")
-
-
-    return text
-
-def name_order(text):
-    localized_name = ["Aoko Aozaki", "Alice Kuonji", "Soujuurou Shizuki", "Touko Aozaki", "Tobimaru Tsukiji",
-                      "Housuke Kinomi", "Kojika Kumari", "Eiri Fumizuka", "Ritsuka Suse", "Yuika Suse",
-                      "Kazuki Yamashiro", "Yurihiko Tokitsu", "Kimikuni Tsukiji", "Hitoyoshi Tsukiji"]
-    correct_name = ["Aozaki Aoko", "Kuonji Alice", "Shizuki Soujuurou", "Aozaki Touko", "Tsukiji Tobimaru",
-                    "Kinomi Housuke", "Kumari Kojika", "Fumizuka Eiri", "Suse Ritsuka", "Suse Yuika",
-                    "Yamashiro Kazuki", "Tokitsu Yurihiko", "Tsukiji Kimikuni", "Tsukiji Hitoyoshi"]
-
-    for i in range (len(localized_name)):
-        line_count = 0
-        for line in text:
-            if (localized_name[i] in line) and (correct_name[i] not in line):
-                text[line_count] = text[line_count].replace(localized_name[i], correct_name[i])
-            line_count += 1
-
-    return text
-
 def honorifics(text_en, text_jp):
     text_en = honorifics_yuika(text_en, text_jp)
     text_en = honorifics_yamashiro(text_en, text_jp)
@@ -177,23 +54,25 @@ def honorifics(text_en, text_jp):
     return text_en
 
 def honorifics_special(text_en, text_jp):
-    # Special Cases:
-    # アッちゃん - Allie
-    # アコちゃん - Aoko
-    # ペン太くん - Flippy
-    # 先生 - Sir
-
-    jp_name = ["アッちゃん", "アコちゃん", "ペン太くん", "先生", "先生", "トコちゃん", "唯ちゃん", "唯ちゃん", "土桔由里彦氏", "土桔由里彦"]
-    en_name_honorific = ["Acchan", "Ako-chan", "Penta-kun", "sensei", "Sensei", "Toko-chan", "Yui-chan", "Yui-chan", "Tokitsu Yurihiko-shi", "Tokitsu Yurihiko"]
-    en_name = ["Allie", "Aoko", "Flippy", "sir", "Sir", "Touko", "Yuika", "Yui", "Mr. Tokitsu", "Mr. Tokitsu"]
-
-    for i in range(0, len(en_name)):
-        line_count = 0
-        for line_jp in text_jp:
-            if jp_name[i] in line_jp:
-                if (en_name[i] in text_en[line_count]) and (en_name_honorific[i] not in text_en[line_count]):
-                    text_en[line_count] = text_en[line_count].replace(en_name[i], en_name_honorific[i])
-            line_count = line_count + 1
+    special_cases = [
+        ("アッちゃん", "Allie", "Acchan"),
+        ("アコちゃん", "Aoko", "Ako-chan"),
+        ("ペン太くん", "Flippy", "Penta-kun"),
+        ("先生", "sir", "sensei"),
+        ("先生", "Sir", "Sensei"),
+        ("トコちゃん", "Touko", "Toko-chan"),
+        ("唯ちゃん", "Yuika", "Yui-chan"),
+        ("唯ちゃん", "Yui", "Yui-chan"),
+        ("土桔由里彦氏", "Mr. Tokitsu", "Tokitsu Yurihiko-shi"),
+        ("土桔由里彦", "Mr. Tokitsu", "Tokitsu Yurihiko"),
+    ]
+    special_cases.sort(key=lambda case: len(case[0]), reverse=True)
+    for jp_name, en_name, en_name_honorific in special_cases:
+        for index, line_jp in enumerate(text_jp):
+            if jp_name not in line_jp:
+                continue
+            if en_name in text_en[index] and en_name_honorific not in text_en[index]:
+                text_en[index] = text_en[index].replace(en_name, en_name_honorific)
 
     # Edge Cases
     # Aozaki introduction => <蒼崎|あおざき><青子|あおこ>くん
@@ -328,9 +207,6 @@ def honorifics_special(text_en, text_jp):
 
 
 def honorifics_yuika(text_en, text_jp):
-    # 1. Map JP text directly to the exact desired EN output.
-    # Order matters: we put the longest/most specific names first
-    # so "シスター唯架" is caught before just "シスター" or "唯架".
     jp_target_mapping = [
         ("シスター唯架", "Sister Yuika"),
         ("周瀬唯架", "Suse Yuika"),
@@ -344,9 +220,6 @@ def honorifics_yuika(text_en, text_jp):
         ("ユイカ", "Yuika"),
         ("シスター", "Sister")
     ]
-
-    # 2. List all the inconsistent ways the English text refers to her.
-    # We include your bugged outputs too, so this script cleans them up if they exist!
     en_aliases = [
         "Sister-san Yuika-san",
         "Yuika Yuika",
@@ -366,119 +239,42 @@ def honorifics_yuika(text_en, text_jp):
         "Suse",
         "Yui"
     ]
-
-    # Sort the English aliases by length (longest first) so regex checks "Sister Yuika" before "Sister"
     en_aliases_sorted = sorted(en_aliases, key=len, reverse=True)
-
-    # Build a regex pattern to match these exactly as whole words.
-    # This is case-sensitive by default, which safely ignores common nouns like "my sister".
     pattern = r'\b(?:' + '|'.join(map(re.escape, en_aliases_sorted)) + r')\b'
     alias_regex = re.compile(pattern)
-
     for i in range(len(text_jp)):
         line_jp = text_jp[i]
-
-        # Figure out the true intended name from the Japanese text
         target_en = None
         for jp_term, expected_en in jp_target_mapping:
             if jp_term in line_jp:
                 target_en = expected_en
                 break
-
         if target_en:
-            # Find any of the recognized English aliases in the line and swap them for the correct target
             text_en[i] = alias_regex.sub(target_en, text_en[i])
 
     return text_en
 
 def honorifics_yamashiro(text_en, text_jp):
-    # 1. Map JP text directly to the exact desired EN output.
-    # Order matters: we put the longest/most specific names first
-    # so "シスター唯架" is caught before just "シスター" or "唯架".
     jp_target_mapping = [
         ("山城教諭", "Mr. Yamashiro"),
         ("山城先生", "Yamashiro-sensei"),
         ("山城", "Yamashiro")
     ]
-
-    # 2. List all the inconsistent ways the English text refers to her.
-    # We include your bugged outputs too, so this script cleans them up if they exist!
-    en_aliases = [
-        "Mr. Yamashiro"
-    ]
-
-    # Sort the English aliases by length (longest first) so regex checks "Sister Yuika" before "Sister"
+    en_aliases = ["Mr. Yamashiro" ]
     en_aliases_sorted = sorted(en_aliases, key=len, reverse=True)
-
-    # Build a regex pattern to match these exactly as whole words.
-    # This is case-sensitive by default, which safely ignores common nouns like "my sister".
     pattern = r'\b(?:' + '|'.join(map(re.escape, en_aliases_sorted)) + r')\b'
     alias_regex = re.compile(pattern)
-
     for i in range(len(text_jp)):
         line_jp = text_jp[i]
-
-        # Figure out the true intended name from the Japanese text
         target_en = None
         for jp_term, expected_en in jp_target_mapping:
             if jp_term in line_jp:
                 target_en = expected_en
                 break
-
         if target_en:
-            # Find any of the recognized English aliases in the line and swap them for the correct target
             text_en[i] = alias_regex.sub(target_en, text_en[i])
 
     return text_en
-
-def americanisms(text):
-
-    # It's weird, in these two instances they use an americanised school grade system, but in all other instances so
-    # far they stick to the Japanese one
-    # There is one more instance, but I don't think I'll really add this to the patch. This function is never called, btw
-
-    # 11th Grade => 2nd Year
-    text[559] = text[559].replace("11th grade", "2nd year")
-    # Freshman -> 1st Year
-    text[2387] = text[2387].replace("freshman", "first-year")
-
-    return text
-
-def translation_mistakes(text):
-
-    # No reason not to fix these
-
-    # Get him out of his head
-    text[964] = text[964].replace("get him out of his head", "get him out of her head")
-    # Missing "to"
-    text[2792] = text[2792].replace("use them", "use them to")
-    # To look of pity -> the look of pity
-    text[3395] = text[3395].replace("to look", "the look")
-    # missing "the"
-    text[3437] = text[3437].replace("unassuming", "the unassuming")
-    # missing "the"
-    text[3471] = text[3471].replace("keeper", "the keeper")
-    # You're weren't
-    text[3747] = text[3747].replace("You're", "You")
-    # missing "to"
-    text[3898] = text[3898].replace("seemed", "seemed to")
-    # Alice -> Aoko ### How do you make an error like this? Don't they have proofreaders? And they never fixed this?!
-    text[14419] = text[14419].replace("Alice", "Aoko")
-    # missing "m"
-    text[15170] = text[15170].replace("with y mouth", "with my mouth")
-    # missing "throw"
-    text[16504] = text[16504].replace("to those", "to throw those")
-    # missing "what"
-    text[18823] = text[18823].replace("people", "what people")
-    # missing "Aozaki". Not really an error, but I would consider a mistake since the contrast was intended,
-    text[19515] = text[19515].replace("so mad", "so mad, Aozaki")
-    # he -> she
-    text[21765] = text[21765].replace("then he'd", "then she'd")
-    # missing "be"
-    text[21768] = text[21768].replace("had to unlucky", "had to be unlucky")
-
-    return text
-
 
 class Replacer:
     def __init__(self, game_dir, input_path, output_path):
@@ -486,12 +282,9 @@ class Replacer:
         self.out_path = os.path.join(game_dir, output_path)
 
     def get_script(self):
-        script = open(self.in_path, "r", encoding='utf-8')
-        text = script.readlines()
-        script.close()
-        return text
+        with open(self.in_path, "r", encoding="utf-8") as script:
+            return script.readlines()
 
     def set_script(self, text):
-        fout = open(self.out_path, 'w', encoding='utf-8')
-        fout.writelines(text)
-        fout.close()
+        with open(self.out_path, "w", encoding="utf-8") as fout:
+            fout.writelines(text)
